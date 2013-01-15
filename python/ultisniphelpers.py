44e90333efdca1e99a17d3c732e425f4746716d5
Dane Summers <dsummersl@yahoo.com>
2013-01-15 09:53:20 -0500

    custom python methods for my ultisnips snippets

--- python/ultisniphelpers.py
+++ python/ultisniphelpers.py
@@ -1,4 +1,18 @@
 def complete(t,opts):
+  """ how to call this:
+
+  global !p
+  import os
+  import sys
+  sys.path.append(os.path.expanduser('~/.vim/python'))
+  from ultisniphelpers import *
+  endglobal
+
+  snippet '^t' "Example completion snippet" r
+  $1`!p snip.rv=complete(t[1],['one','two','three','overboard'])`
+  endsnippet
+  """
+
   if t:
     opts = [ m[len(t):] for m in opts if m.startswith(t) ]
   if len(opts) == 1:
